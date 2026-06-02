de6d29136d12a449fb5e11d253c337752277be35
runoneall <runoobsteve@gmail.com>
2026-06-03 07:41:31 +0800

    feat: implement authentication routes with login, status, and logout endpoints

--- src/api.js
+++ src/api.js
@@ -20,9 +20,11 @@ const api = express.Router()
 app.use('/api', api)
 
 import { getenv } from './cfenv.js'
+const env = getenv()
+
 api.use((req, res, next) => {
-    const env = getenv()
     if (typeof env.password !== 'string') return res.status(500).send('密码未设置')
+    if (typeof env.jwt_secret !== 'string') return res.status(500).send('jwt 密钥未设置')
     if (typeof env.data === 'undefined') return res.status(500).send('存储未绑定')
     return next()
 })
@@ -30,6 +32,9 @@ api.use((req, res, next) => {
 api.get('/ping', (req, res) => {
     return res.status(200).send('pong')
 })
+
+import auth from './apis/auth.js'
+api.use('/auth', auth)
 
 import { createServer, get } from 'node:http'
 import { httpServerHandler } from 'cloudflare:node'
