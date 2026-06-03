efbfbe6edda3568dc9ab5f53278ac8f7936345b1
runoneall <runoobsteve@gmail.com>
2026-06-03 10:18:02 +0800

    feat: remove Pinia dependency and related initialization from main.js

--- web/src/main.js
+++ web/src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 const app = createApp(App)
 
-import { createPinia } from 'pinia'
-app.use(createPinia())
-
 import router from '@/router.js'
 app.use(router)
 
