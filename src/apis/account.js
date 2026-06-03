b73c33de1f47b5ae98d0cc57bb03b00183170ef1
runoneall <runoobsteve@gmail.com>
2026-06-03 17:02:09 +0800

    feat: add account info retrieval and setting functionality with validation

--- src/apis/account.js
+++ src/apis/account.js
@@ -17,4 +17,42 @@ account.get('/list', needauth, async (req, res) => {
     })
 })
 
+account.get('/info', needauth, async (req, res) => {
+    const name = req.query.name
+    if (typeof name !== 'string' || name === '') {
+        return res.status(400).send('请求错误')
+    }
+
+    const info = await env.data.get(name)
+    if (info === null) {
+        return res.status(404).send('信息未找到')
+    }
+
+    const obj = JSON.parse(info)
+    return res.status(200).json(obj)
+})
+
+account.post('/set', needauth, async (req, res) => {
+    const name = req.query.name
+    if (typeof name !== 'string' || name === '') {
+        return res.status(400).send('请求错误')
+    }
+
+    const { username, password, totp, note } = req.body
+    const isok = typeof username === 'string' && username !== '' && typeof password === 'string' && password !== '' && typeof totp === 'string' && typeof note === 'string'
+    if (!isok) {
+        return res.status(400).send('数据错误')
+    }
+
+    const raw = JSON.stringify({
+        username,
+        password,
+        totp,
+        note
+    })
+
+    await env.data.put(name, raw)
+    return res.status(200).send('设置成功')
+})
+
 export default account
